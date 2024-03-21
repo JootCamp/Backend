513ace7a5537254a38143548d155a60e8669d531
jgm0327 <gmjang6349@gmail.com>
2024-03-21 10:32:45 +0900

    feat: 초기 설정
    프로젝트를 위한 필수 dependency 추가

---
+++ src/main/java/com/jootcamp/superboard/SuperboardApplication.java
@@ -0,0 +1,13 @@
+package com.jootcamp.superboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jootcamp/superboard/SuperboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jootcamp.superboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
