13bc15ad97b275e3a4440331a43078a629619e04
Jennorresothie <head7579@gmail.com>
2024-05-11 22:07:15 +0900

    feat(BE) : 로그인 확인 필터

--- src/main/java/com/jootcamp/superboard/SuperboardApplication.java
+++ src/main/java/com/jootcamp/superboard/SuperboardApplication.java
@@ -2,10 +2,12 @@ package com.jootcamp.superboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@ServletComponentScan
 public class SuperboardApplication {
 
     public static void main(String[] args) {
