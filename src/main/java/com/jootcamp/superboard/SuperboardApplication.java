16fb28141fd9f1e9dad6b882d08a380e4d958c63
Jennorresothie <head7579@gmail.com>
2024-05-18 15:40:05 +0900

    feat(BE) : cors 설정

--- src/main/java/com/jootcamp/superboard/SuperboardApplication.java
+++ src/main/java/com/jootcamp/superboard/SuperboardApplication.java
@@ -3,7 +3,10 @@ package com.jootcamp.superboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -14,4 +17,13 @@ public class SuperboardApplication {
         SpringApplication.run(SuperboardApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 }
