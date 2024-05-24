faa3f7072dd2d81801c43936d7f47546271f80de
Jennorresothie <head7579@gmail.com>
2024-05-24 16:00:38 +0900

    feat(BE) : #18 OncePerRequestFilter 상속받고 shouldNotFilter 적용

--- src/main/java/com/jootcamp/superboard/common/filter/LoginCheckFilter.java
+++ src/main/java/com/jootcamp/superboard/common/filter/LoginCheckFilter.java
@@ -5,28 +5,27 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Component;
-import org.springframework.util.PatternMatchUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 @Component
-public class LoginCheckFilter implements Filter {
-    private static final String[] whitelist = {"/", "/login", "/logout","/css/*"};
-
-
+public class LoginCheckFilter extends OncePerRequestFilter {
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         String requestURI = httpRequest.getRequestURI();
         HttpServletResponse httpResponse = (HttpServletResponse) response;
 
         try {
-            if (isLoginCheckPath(requestURI)) {
+            if (!shouldNotFilter(request)) {
+                System.out.println("통과");
                 HttpSession session = httpRequest.getSession(false);
                 if (session == null || session.getAttribute("userId") == null) {
                     System.out.println("미인증 사용자 요청 {}" + requestURI);
                     //로그인으로 redirect
-                    //httpResponse.sendRedirect("/login?redirectURL=" + requestURI);
+                    httpResponse.setStatus(401);
                     return; //여기가 중요, 미인증 사용자는 다음으로 진행하지 않고 끝!
                 }
             }
@@ -37,13 +36,18 @@ public class LoginCheckFilter implements Filter {
         } finally {
             System.out.println("인증 체크 필터 종료 {}" + requestURI);
         }
+
     }
 
     /**
      * 화이트 리스트의 경우 인증 체크X
      */
-    private boolean isLoginCheckPath(String requestURI) {
-        return !PatternMatchUtils.simpleMatch(whitelist, requestURI);
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        String[] excludePath = {"/", "/login", "/logout","/css/*", "/signup"};
+        String path = request.getRequestURI();
+        System.out.println("snf "+path);
+        return Arrays.stream(excludePath).anyMatch(path::startsWith);
     }
 
 }
