3bd88dd599d79ed2b2fc7c5f046a6ebe3875fff5
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-06-08 22:29:48 +0900

    Feature/18 (#31)
    * feat(BE) : #18 OncePerRequestFilter 상속받고 shouldNotFilter 적용
    
    * fix(BE) : 로그 추가 및 수정
    
    * fix(BE) : #18 피드백 받은거 수정

--- src/main/java/com/jootcamp/superboard/common/filter/LoginCheckFilter.java
+++ src/main/java/com/jootcamp/superboard/common/filter/LoginCheckFilter.java
@@ -5,45 +5,47 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Component;
-import org.springframework.util.PatternMatchUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 @Component
-public class LoginCheckFilter implements Filter {
-    private static final String[] whitelist = {"/", "/login", "/logout","/css/*"};
-
-
+public class LoginCheckFilter extends OncePerRequestFilter {
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        String requestURI = httpRequest.getRequestURI();
-        HttpServletResponse httpResponse = (HttpServletResponse) response;
-
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+        String requestURI = request.getRequestURI();
         try {
-            if (isLoginCheckPath(requestURI)) {
-                HttpSession session = httpRequest.getSession(false);
+            if (!shouldNotFilter(request)) {
+                System.out.println("통과");
+                HttpSession session = request.getSession(false);
                 if (session == null || session.getAttribute("userId") == null) {
                     System.out.println("미인증 사용자 요청 {}" + requestURI);
                     //로그인으로 redirect
-                    httpResponse.sendError(401, "로그인 필요");
+                    response.sendError(401, "로그인 필요");
                     return; //여기가 중요, 미인증 사용자는 다음으로 진행하지 않고 끝!
                 }
             }
+
             chain.doFilter(request, response); //다음 필터 진행. 없다면 서블릿 띄우기
         } catch (Exception e) {
             e.printStackTrace();
             throw e;
         } finally {
-            System.out.println("인증 체크 필터 종료 {}" + requestURI);
+            System.out.println("인증 체크 필터 종료 :" + requestURI);
         }
+
     }
 
     /**
      * 화이트 리스트의 경우 인증 체크X
      */
-    private boolean isLoginCheckPath(String requestURI) {
-        return !PatternMatchUtils.simpleMatch(whitelist, requestURI);
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        String[] excludePath = {"/", "/login", "/logout", "/css/*", "/signup"};
+        String path = request.getRequestURI();
+        System.out.println("snf " + path);
+        return Arrays.stream(excludePath).anyMatch(path::equals);
     }
 
 }
