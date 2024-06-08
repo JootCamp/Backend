4a6b8ec1787eff33de61035bffc08fc905a42ba8
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-06-08 22:41:11 +0900

    feat(BE) :  글로버예외추가 BadRequestException (#32)

--- src/main/java/com/jootcamp/superboard/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jootcamp/superboard/common/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -12,5 +13,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> notFoundHandler(NotFoundException e) {
         ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> badRequestHandler(BadRequestException bad){
+        ErrorResponse errorResponse = new ErrorResponse(bad.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 }
