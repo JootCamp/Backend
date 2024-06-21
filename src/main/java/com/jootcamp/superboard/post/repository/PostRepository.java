97b6339821b42e11277dbf777ed60fdd533f3be5
Jennorresothie <head7579@gmail.com>
2024-06-21 16:45:59 +0900

    #34 게시판 목록 조회, 게시판 조회 기능
    - Controller
    - Repository
    PostNotFoundException
    -Service

--- src/main/java/com/jootcamp/superboard/post/repository/PostRepository.java
+++ src/main/java/com/jootcamp/superboard/post/repository/PostRepository.java
@@ -3,5 +3,10 @@ package com.jootcamp.superboard.post.repository;
 import com.jootcamp.superboard.post.repository.entity.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
+    Optional<PostEntity> findByIdAndIsDeletedIsFalse(Long postId);
+    List<PostEntity> findAllByIsDeletedIsFalse();
 }
--- src/main/java/com/jootcamp/superboard/post/service/PostService.java
+++ src/main/java/com/jootcamp/superboard/post/service/PostService.java
@@ -2,10 +2,13 @@ package com.jootcamp.superboard.post.service;
 
 import com.jootcamp.superboard.post.repository.PostRepository;
 import com.jootcamp.superboard.post.repository.entity.PostEntity;
+import com.jootcamp.superboard.post.repository.execption.PostNotFoundException;
 import com.jootcamp.superboard.post.service.dto.Post;
 import com.jootcamp.superboard.post.service.dto.UpsertPost;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +17,20 @@ public class PostService {
 
     public Post create(UpsertPost upsertPost) {
         PostEntity post = postRepository.save(upsertPost.toEntity());
+        return Post.from(post);
+    }
+
+    public List<Post> findAll() {
+        List<PostEntity> posts = postRepository.findAllByIsDeletedIsFalse();
+        return posts.stream()
+                .map(Post::from)
+                .toList();
+    }
+
+    public Post findById(long postId) {
+        PostEntity post = postRepository.findByIdAndIsDeletedIsFalse(postId)
+                .orElseThrow(()->new PostNotFoundException(postId));
+
         return Post.from(post);
     }
 }
