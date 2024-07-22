b161d38ffe60d4b73f2fbc85c4385e05ccd26641
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-07-22 11:00:13 +0900

    Feature/53 (#54)
    * #40 common의 baseEntiry 패키지와 패키지내 하위클래스 이름 변경
    
    baseEntity -> entity
    BaseTimeEntity -> BaseEntity
    으로 변경
    
    * #43 user argumnet resolver 구현 (#47)
    
    * #43 user argumnet resolver 구현
    
    - 어노테이션 생성
    UserInfo
    
    - User정보 담는 DTO생성
    UserInfo
    
    - setArrtibute UserInfo 객체로 저장
    - WebCongfig 에 Resolver 추가
    - 각 Controller userId 부분 코드 변경
    - UserNotFoundException 기본 생성자 추가
    
    * Refactor(BE) : #43 피드백 반영한 수정
    
    1. "userInfo" Constants 로 사용
    변경 내역
    - UserConstant
    - LoginCheckFilter
    - UserArgumentResolver
    - UserService
    
    2. UserNotFoundException -> UserEmailNotFoundException 로 클래스 명 변경
    
    * Revert "Refactor(BE) : #43 피드백 반영한 수정"
    
    This reverts commit 315f6d80abbff9b9b728ac52fef6398799474581.
    
    * Reapply "Refactor(BE) : #43 피드백 반영한 수정"
    
    This reverts commit 69936bbdbce82d8fd8b0a3ea86782b48ed8b980c.
    
    * Auto stash before checking out "HEAD"
    
    * fix(BE) : #53
    
    # LoginFilter
    - shouldNotFilter() 메소드에 "/board", "/post" 으로 GET 요청시 true값 반환하도록 수정
    - System.out,println 및 코드 정리
    
    # gitignore 추가
    - 테스트 코드 안올라가게 경로 추가
    
    * fix(BE) : #53 피드백 수정
    
    ## gitignore
    - gitignore에 testcode 제외 제외

--- src/main/java/com/jootcamp/superboard/common/filter/LoginCheckFilter.java
+++ src/main/java/com/jootcamp/superboard/common/filter/LoginCheckFilter.java
@@ -16,24 +16,21 @@ import static com.jootcamp.superboard.common.constants.UserConstant.USER_INFO;
 public class LoginCheckFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
-        String requestURI = request.getRequestURI();
-        try {
-                System.out.println("통과");
-                HttpSession session = request.getSession(false);
-                if (session == null || session.getAttribute(USER_INFO) == null) {
-                    System.out.println("미인증 사용자 요청 {}" + requestURI);
-                    //로그인으로 redirect
-                    response.sendError(401, "로그인 필요");
-                    return; //여기가 중요, 미인증 사용자는 다음으로 진행하지 않고 끝!
-                }
 
+        try {
+            HttpSession session = request.getSession(false); // 세션이 존재하는지 확인하고, 존재하면 세션 객체를 반환하고, 존재하지 않으면 null을 반환하는 용도
+
+            if (session == null || session.getAttribute(USER_INFO) == null) {
+                response.sendError(401, "로그인 필요");
+                return; //여기가 중요, 미인증 사용자는 다음으로 진행하지 않고 끝!
+            }
 
             chain.doFilter(request, response); //다음 필터 진행. 없다면 서블릿 띄우기
         } catch (Exception e) {
             e.printStackTrace();
             throw e;
         } finally {
-            System.out.println("인증 체크 필터 종료 :" + requestURI);
+            System.out.println("인증 체크 필터 종료 :" + request.getRequestURI());
         }
 
     }
@@ -43,9 +40,14 @@ public class LoginCheckFilter extends OncePerRequestFilter {
      */
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
-        String[] excludePath = {"/login", "/logout", "/css/*", "/signup", "/swagger", "/v3"};
         String path = request.getRequestURI();
-        System.out.println("snf " + path);
+
+        if (path.startsWith("/board") || path.startsWith("/post")) {
+            return request.getMethod().equals("GET");
+        }
+
+        String[] excludePath = {"/login", "/logout", "/css/*", "/signup", "/swagger", "/v3"};
+
         return Arrays.stream(excludePath).anyMatch(path::startsWith);
     }
 
