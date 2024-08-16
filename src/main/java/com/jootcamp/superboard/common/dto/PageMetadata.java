212541d9d6974552fedcd15d2bb9a2e00a0c2ff3
Jennorresothie <head7579@gmail.com>
2024-08-16 21:22:39 +0900

    fix: 66번 issue PageMetadata of 메소드 생성
    PageMetadata 제네릭으로 of 함수로 생성하도록 변경
    - postService
    - commentService

--- src/main/java/com/jootcamp/superboard/common/dto/PageMetadata.java
+++ src/main/java/com/jootcamp/superboard/common/dto/PageMetadata.java
@@ -1,6 +1,8 @@
 package com.jootcamp.superboard.common.dto;
 
 import lombok.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 @Getter
 @Setter
@@ -12,4 +14,13 @@ public class PageMetadata {
     private int size;
     private long totalCount;
     private int totalPageCount;
+
+    public static <T> PageMetadata of(Pageable pageable, Page<T> pages){
+        return PageMetadata.builder()  // 타입 명시적 처리
+                .currentPage(pageable.getPageNumber())
+                .size(pages.getSize())
+                .totalCount(pages.getTotalElements())
+                .totalPageCount(pages.getTotalPages())
+                .build();
+    }
 }
