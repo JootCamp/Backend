1d606ad6b89e0b93b179c0cdc99643520324e06e
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-08-24 21:11:02 +0900

    fix - issue 81 : CommentApiController 수정 (#83)
    * fix(BE): #69 PageDto로 이름 수정
    
    PageDto와 관련된 클래스들
    
    * CommentPage
    * PageResponse
    * PostPage
    
    * fix - issue 69 : PageDto로 수정
    
    Dto 삭제
    - CommentPage
    - PostPage
    
    PageDto를 interface -> class 변경
    
    코드수정
    - CommentService
    - PostService
    
    * fix - issue 69
    
    * fix - issue 81 : CommentApiController 수정
    
    GET, DELETE 요청에서 @RequsetBody UpsertComment 부분 삭제
    
    ---------
    
    Co-authored-by: jgm0327 <gmjang6349@gmail.com>

--- src/main/java/com/jootcamp/superboard/comment/controller/CommentApiController.java
+++ src/main/java/com/jootcamp/superboard/comment/controller/CommentApiController.java
@@ -40,8 +40,7 @@ public class CommentApiController {
     @GetMapping("/boards/{boardId}/posts/{postId}/comments")
     @Operation(summary = "댓글 조회", description = "해당 게시글에 대한 댓글 조회")
 
-    public ResponseEntity<PageResponse<Comment>> getComments(@RequestBody UpsertCommentRequest request,
-                                                             @PathVariable("boardId") long boardId,
+    public ResponseEntity<PageResponse<Comment>> getComments(@PathVariable("boardId") long boardId,
                                                              @PathVariable("postId") long postId,
                                                              Pageable pageable) {
         PageResponse<Comment> comments = PageResponse.from(commentService.getComments(postId, pageable));
@@ -65,8 +64,7 @@ public class CommentApiController {
     @DeleteMapping("/boards/{boardId}/posts/{postId}/comments/{commentId}")
     @Operation(summary = "댓글 삭제", description = "commentId에 해당하는 댓글 삭제")
 
-    public ResponseEntity<List<CommentResponse>> deleteComment(@RequestBody UpsertCommentRequest request,
-                                                               @PathVariable("boardId") long boardId,
+    public ResponseEntity<List<CommentResponse>> deleteComment(@PathVariable("boardId") long boardId,
                                                                @PathVariable("postId") long postId,
                                                                @PathVariable("commentId") long commentId,
                                                                @UserInfo AuthUser user) {
